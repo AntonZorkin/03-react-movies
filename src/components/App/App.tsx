42fda077e2237f60c334f3c95192c4ec6196f277
Anton Zorkin <zanton1978@gmail.com>
2025-09-25 10:06:32 +0200

    commit message

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,20 @@
+import SearchBar from "../SearchBar/SearchBar";
 import css from "./App.module.css";
-import CafeInfo from "../CafeInfo/CafeInfo";
-import VoteOptions from "../VoteOptions/VoteOptions";
-import { useState } from "react";
-import type { VoteType, Votes } from "../../types/votes";
-import VoteStats from "../VoteStats/VoteStats";
-import Notification from "../Notification/Notification";
+
+// import { useState } from "react";
+
+
+
 
 const App = () => {
-  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
-  const handleVote = (type: VoteType) => {
-    setVotes((prev) => ({ ...prev, [type]: prev[type] + 1 }));
-  };
-  const resetVotes = () => {
-    setVotes({ good: 0, neutral: 0, bad: 0 });
-  };
-  const totalVotes = votes.bad + votes.good + votes.neutral;
-  const positiveRate = totalVotes ? Math.round((votes.good / totalVotes)*100) : 0;
+  const handleSubmit = (query: string) => {
+    console.log(query);
+    
+  }
   return (
     <div className={css.app}>
-      <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
-      {totalVotes > 0 ?
-        <VoteStats
-          votes={votes}
-          totalVotes={totalVotes}
-          positiveRate={positiveRate}
-        />:<Notification/>}
+      <SearchBar onSubmit={handleSubmit}/>
+      
     </div>
   );
 };
